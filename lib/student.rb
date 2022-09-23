158e5ff429f25865312be5cbeaccbfa46c4b2714
Joseph <jsltran@gmail.com>
2022-09-22 17:24:44 -0700

    commit tree eed4b9bee44c09fbee3bd3e9cbb2da9db79a937c

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
 
+
+class Student < User
+    # @KNOWLEDGE = []
+    def initialize
+        @knowledge = Array.new
+    end 
+
+    def learn(lesson)
+        @knowledge << lesson   
+    end 
+
+    def knowledge
+        return @knowledge
+    end 
 end
